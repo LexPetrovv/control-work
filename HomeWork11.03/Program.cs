35cb56f106e14398d9548d401bfbb0b983c4103b
Алексей <156588887+LexPetrovv@users.noreply.github.com>
2024-03-11 19:48:50 +0300

    add Program
    Добавлена программа для решения задачи

---
+++ HomeWork11.03/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] originalArray =  { "Hello", "2", "world", ":-)" };                              
+        string[] newArray = new string[originalArray.Length];
+        int newIndex = 0;
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            if(originalArray[i].Length <= 3)
+            {
+                newArray[newIndex] = originalArray[i];
+                newIndex++;
+            }
+        }
+        Console.WriteLine("Новый массив строк:");
+        for (int i = 0; i < newIndex; i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
+    }
+}
+
+
